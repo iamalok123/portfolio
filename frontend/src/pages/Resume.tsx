c52c534107415bd0345e6f83df141a286440d77b
Alok Hotta <alokhotta10@gmail.com>
2026-06-03 20:58:39 +0530

    fix: replace corrupted resume.pdf

--- frontend/src/pages/Resume.tsx
+++ frontend/src/pages/Resume.tsx
@@ -6,7 +6,10 @@ import 'react-pdf/dist/Page/AnnotationLayer.css'
 import 'react-pdf/dist/Page/TextLayer.css'
 import { PageTransition } from '../components/layout/PageTransition'
 
-pdfjs.GlobalWorkerOptions.workerSrc = `https://unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  'pdfjs-dist/build/pdf.worker.min.mjs',
+  import.meta.url,
+).toString()
 
 const RESUME_URL = '/resume.pdf'
 
